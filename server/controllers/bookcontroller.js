2be248c3967a10613ce1a96aa15a88edfa159088
Mustafa <mustafaalibekov04@gmail.com>
2025-06-25 13:33:47 +0300

    adding two book function and delete nodemodules

--- server/controllers/bookcontroller.js
+++ server/controllers/bookcontroller.js
@@ -30,3 +30,27 @@ exports.getBook = async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 };
+
+exports.deleteBook = async (req, res) => 
+    {
+        const {bookID} = req.body;
+        try
+        {
+            const result = await pool.query('DELETE FROM books WHERE bookID = $1', [bookID]);
+            res.sendStatus(204); // No Content
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+    }
+
+ exports.updateBook = async (req, res) => 
+    {
+        const {userID, username, login, password, role} = req.body;
+        try
+        {
+            const result = await pool.query('Update books SET bookname = $1, imagelink=$2, rateid=$3, categoryid = $4, authorid = $5, booklink = $6 WHERE "bookID" = $5',[bookname, imagelink, rateid, categoryid, authorid, booklink]);
+        res.status(200).json(result.rows[0]); // Всегда используй json()
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+    }
--- server/routes/bookrouter.js
+++ server/routes/bookrouter.js
@@ -4,5 +4,7 @@ const router= express.Router();
 
 router.post('/books', bookController.createBook);
 router.post('/getBook', bookController.getBook);
+router.post('/deleteBook', bookController.deleteBook);
+router.post('/updateBook', bookController.updateBook);
 
 module.exports = router;
